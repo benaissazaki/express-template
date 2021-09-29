cb224e55658f07777a66c0e00e65472281b34c1e
lilxak <zakben45@gmail.com>
2021-09-29 15:38:26 +0100

    Add example route

--- app.js
+++ app.js
@@ -9,4 +9,9 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
+// Routes
+app.get("/", (req, res) => {
+    res.contentType("html").status(200).send("<h1>Hello world</h1>")
+})
+
 module.exports = app
