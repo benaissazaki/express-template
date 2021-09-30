0f9a3b3c1b372992265457cc3baaa1751fc529cd
lilxak <zakben45@gmail.com>
2021-09-30 15:39:55 +0100

    Alter exampleSchema's toJSON method

--- models/example.js
+++ models/example.js
@@ -11,4 +11,11 @@ const exampleSchema = new mongoose.Schema({
   }
 })
 
+exampleSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
 module.exports = mongoose.model('Example', exampleSchema)
